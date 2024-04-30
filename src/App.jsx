f4c02fde6e078ec2141ef4743840a8d46312d984
Guhan-M <guhan76guhan@gmail.com>
2024-04-30 23:44:07 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import AppRoutes from './utils/AppRoutes'
-export const API_URL = 'http://localhost:8000'
+export const API_URL = 'https://resume-builder-backend-guq3.onrender.com'
 import './index.css'
 function App() {
   const router= createBrowserRouter(AppRoutes)
